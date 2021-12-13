17ad885df156d713281fccb62dd1f50491dd6938
anatoly.sakhno <anatoly.sakhno@gmail.com>
2021-12-13 11:16:56 +0200

    syntax corrections

--- tdta/ApplicationFacade.java
+++ tdta/ApplicationFacade.java
@@ -1,17 +1,28 @@
 package tdta;
 
 // WARNING: Don't keep all the application functions
-// in a single facade 
-Interface ApplicationFacade {
+// in a single facade: use Additional Facades for different features
+interface ApplicationFacade {
     void registerUser(User user);
+
     void unregisterUser(User user);
+
     void login(User user);
+
     void logout();
+
     void createSomething(Something something);
+
     void modifySomething(Something something);
+
     void deleteSomething(UUID somethingUuid);
 }
 
-public class FakeApplication implements ApplicationFacade{}
-public class ApiApplication implements ApplicationFacade{}
-public class UiApplication implements ApplicationFacade{}
+public class UiApplication implements ApplicationFacade {
+} // End-2-End level
+
+public class ApiApplication implements ApplicationFacade {
+} // Integration level
+
+public class FakeApplication implements ApplicationFacade {
+} // Unit/Validation level
